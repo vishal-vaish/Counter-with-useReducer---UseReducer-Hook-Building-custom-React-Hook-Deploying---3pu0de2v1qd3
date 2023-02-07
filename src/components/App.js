aa04d137d3a3289a6b0ccbe0f7f2c8178435d1e5
Vishal Vaish <114350565+vishal-vaish@users.noreply.github.com>
2023-02-07 14:59:37 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,18 @@
 import React, { useReducer } from 'react'
-import { counterReducer } from '../reducers/counterReducer';
-import '../styles.css';
+// import { counterReducer } from '../reducers/counterReducer';
+// import '../styles.css';
+
+const counterReducer = (state, action) => {
+  console.log(state, action);
+  if (action.type === "increment") {
+    return state + 1;
+  }
+  if (action.type === "decrement") {
+    return state - 1;
+  }
+  return state;
+};
+
 const App = () => {
 const count=0;
 const [counter,dispatch] = useReducer(counterReducer,count)
